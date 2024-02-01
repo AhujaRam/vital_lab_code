eab23c92622714dcbbbfc283ad81a6c608f51ad9
AhujaRam <116038576+AhujaRam@users.noreply.github.com>
2024-02-01 18:02:27 -0500

    Add files via upload

---
+++ classification_vs_memory/upper_triangle.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+
+def upper(df):
+    '''Returns the upper triangle of a correlation matrix.
+    You can use scipy.spatial.distance.squareform to recreate matrix from upper triangle.
+    Args:
+      df: pandas or numpy correlation matrix
+    Returns:
+      list of values from upper triangle
+    '''
+    try:
+        assert(type(df)==np.ndarray)
+    except:
+        if type(df)==pd.DataFrame:
+            df = df.values
+        else:
+            raise TypeError('Must be np.ndarray or pd.DataFrame')
+    mask = np.triu_indices(df.shape[0], k=1)
+    return df[mask]
