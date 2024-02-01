d9980f5a0f7ffaaa044777bb51242132b62cb7d7
AhujaRam <116038576+AhujaRam@users.noreply.github.com>
2024-02-01 18:10:49 -0500

    Add files via upload

---
+++ RDM Comparisons/rdm_calc_function.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+from scipy.spatial import distance
+import pandas as pd
+import math
+
+def activity_vector_rdm(activity_vectors):
+    num_vectors = len(activity_vectors)
+    dissimilarity_matrix = np.full((num_vectors, num_vectors), np.nan)
+
+    for i in range(num_vectors):
+        dissimilarity_matrix[i, i] = 0.0  # Set the diagonal values to 0
+
+        for j in range(i + 1, num_vectors):
+            correlation = np.corrcoef(activity_vectors[i], activity_vectors[j])[0, 1]
+            dissimilarity = 1.0 - correlation
+            dissimilarity_matrix[i, j] = dissimilarity
+            dissimilarity_matrix[j, i] = dissimilarity
+
+    df = pd.DataFrame(dissimilarity_matrix)
+    
+    return dissimilarity_matrix
+
